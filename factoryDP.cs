6ea175d5871b24c4c8a5b7f71b5bd9f4c436b3e1
kj27july <79355765+kj27july@users.noreply.github.com>
2021-03-12 13:22:18 +0530

    Tax part
    I have added tax related data

--- factoryDP.cs
+++ factoryDP.cs
@@ -16,5 +16,69 @@ abstract class AbstractClass
 {
     protected double amount = 0;
     public abstract getAmount();
+
+     //add this class(this belongs to Tax)
+        public void calculateAmount(double salary)
+        {
+            Console.WriteLine(amount*salary);
+        }
 }
+
+
+ /*
+    public abstract  class AbstractClass
+    {
+        protected double amount;
+        public abstract void getAmount();
+
+        //add this class
+        public void calculateAmount(double salary)
+        {
+            Console.WriteLine(amount*salary);
+        }
+    }
+    */
+   
+    class Slap1 : AbstractClass
+    {
+        public override void getAmount()
+        {
+            this.amount = 0.05;
+        }
+    }
+
+    class Slap2 : AbstractClass
+    {
+        public override void getAmount()
+        {
+           
+            this.amount = 0.10;
+        }
+    }
+
+    class Slap3 : AbstractClass
+    {
+        public override void getAmount()
+        {
+           
+            this.amount = 0.15;
+        }
+    }
+
+    class Slap4 : AbstractClass
+    {
+        public override void getAmount()
+        {
+           
+            this.amount = 0.20;
+        }
+    }
+    class Slap5 : AbstractClass
+    {
+        public override void getAmount()
+        {
+          
+            this.amount = 0.25;
+        }
+    }
 
