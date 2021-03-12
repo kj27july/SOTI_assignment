98c4e699557d1979de379782a6b3472553aae171
madhaven <jitinjacobgigi@mca.ajce.in>
2021-03-12 11:20:26 +0530

    initial commit

---
+++ factoryDP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class FactoryDP
+{
+	public getClass(string type)
+	{
+        switch (type)
+        {
+            case "Tax": return new Tax();
+                break;
+        }
+	}
+}
+
+abstract class AbstractClass
+{
+    protected double amount = 0;
+    public abstract getAmount();
+}
+
